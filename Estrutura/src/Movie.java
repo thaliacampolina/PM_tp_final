f9f95295fe5ccad423558ab98499b612fa6740b8
Giuliene <giulienesg@yahoo.com.br>
2014-11-05 20:06:58 -0200

    Adicionei o main e fiz algumas modificações no open

--- Estrutura/src/Movie.java
+++ Estrutura/src/Movie.java
@@ -5,17 +5,17 @@ public class Movie {
 	 private int releaseDate;
 	 private int videoReleaseDate;
 	 private int imdbUrl;
-	 private Genere genere;
+	 private Genre genre;
 	
 	 
 	 public Movie(int movieId, String movieTitle, int releaseDate, int videoReleaseDate,
-	  int imdbUrl, Genere genere)
+	  int imdbUrl, Genre genere)
 	 {
 		 this.movieId = movieId;
 		 this.movieTitle = movieTitle;
 		 this.releaseDate = releaseDate;
 		 this.videoReleaseDate = videoReleaseDate;
 		 this.imdbUrl = imdbUrl;
-		 this.genere = genere;		 
+		 this.genre = genere;		 
 	 }
 }
