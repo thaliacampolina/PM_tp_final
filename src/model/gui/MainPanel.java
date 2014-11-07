2288cde4a4ac6a07ed4535b1f6ea242d0ad32e8c
Thalia Campolina <thaliacampolina@hotmail.com>
2014-11-07 00:13:41 -0200

    Modificando para ser UTF-8 e funcionar no terminal

---
+++ src/model/gui/MainPanel.java
@@ -0,0 +1,55 @@
+package model.gui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MainPanel  extends JPanel implements ActionListener
+{
+   private JLabel welcome = new JLabel ("\n  Bem vindo � busca de filmes!\n");
+   private FieldPanel name = new FieldPanel ("Nome do filme: " , 50);
+   private FieldPanel year = new FieldPanel ("Ano: " , 3);
+   private FieldPanel genre = new FieldPanel ("G�nero: " , 10);
+   private FieldPanel rating = new FieldPanel ("Rating de usu�rios (1-5): " , 2);
+   private JButton search = new JButton ("Pesquisar");
+   private JButton clear = new JButton ("Limpar");
+   
+   private JFormattedTextField textField;
+   
+   public MainPanel () 
+   {
+       add (welcome);
+       add (name);
+       add (year);
+       add (genre);
+       add (rating);
+       add (search);
+       add (clear);
+       
+       search.addActionListener(this);
+       clear.addActionListener(this);
+       
+       setLayout (new FlowLayout (FlowLayout.LEFT));
+       
+   }
+   
+   public void actionPerformed (ActionEvent e)
+   {
+       Object obj = e.getSource ();
+       
+       if (obj == search)
+       {
+           // Pegar as coisas dos edit text pra salvar
+       }
+       else if (obj == clear)
+       {
+           name.setTextField ("");
+           year.setTextField ("");
+           genre.setTextField ("");
+           rating.setTextField ("");
+       }
+   }
+}
+
