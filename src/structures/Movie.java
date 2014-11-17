229a6590e95267361acbf5bf3c61b19e6f536ce1
Thalia Campolina <thaliacampolina@hotmail.com>
2014-11-17 00:41:15 -0200

    retorna array string de genre

--- src/structures/Movie.java
+++ src/structures/Movie.java
@@ -1,5 +1,6 @@
 package structures;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 
@@ -32,6 +33,32 @@ public class Movie {
 	 public String getImdbUrl(){return this.imdbUrl;}
 	 public Genre getGenre(){return this.genre;}
 	 public String getAverengeNote(){return this.averengeNote;}
+
+     public ArrayList<String> getAllGenres() {
+         ArrayList<String> arr = new ArrayList<String>();
+         if (this.genre.getUnknown() == 1) { arr.add("unknown"); }
+         if (this.genre.getAction() == 1) { arr.add("action"); }
+         if (this.genre.getAdventure() == 1) { arr.add("adventure"); }
+         if (this.genre.getAnimation() == 1) { arr.add("animation"); }
+         if (this.genre.getChildrens() == 1) { arr.add("children\'s"); }
+         if (this.genre.getComedy() == 1) { arr.add("comedy"); }
+         if (this.genre.getCrime() == 1) { arr.add("crime"); }
+         if (this.genre.getDocumentary() == 1) { arr.add("documentary"); }
+         if (this.genre.getDrama() == 1) { arr.add("drama"); }
+         if (this.genre.getFantasy() == 1) { arr.add("fatasy"); }
+         if (this.genre.getFilmNoir() == 1) { arr.add("film noir"); }
+         if (this.genre.getHorror() == 1) { arr.add("horror"); }
+         if (this.genre.getMusical() == 1) { arr.add("musical"); }
+         if (this.genre.getMystery() == 1) { arr.add("mistery"); }
+         if (this.genre.getRomance() == 1) { arr.add("romance"); }
+         if (this.genre.getSciFi() == 1) { arr.add("sci fi"); }
+         if (this.genre.getThriller() == 1) { arr.add("thriller"); }
+         if (this.genre.getWar() == 1) { arr.add("war"); }
+         if (this.genre.getWestern() == 1) { arr.add("western"); }
+         return arr;
+
+     }
+
 	 
 	public void averageNoteUser(Collection<Data> data)
 	{
